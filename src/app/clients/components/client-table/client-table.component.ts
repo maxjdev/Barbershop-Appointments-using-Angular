1fc24f0dda9a9b14a3a8adb6d3e148ab7bb1490d
maxjdev <maxjramosdev@gmail.com>
2025-03-19 20:52:43 -0300

    feat/implement client table with CRUD actions and dialog integration

--- src/app/clients/components/client-table/client-table.component.ts
+++ src/app/clients/components/client-table/client-table.component.ts
@@ -1,11 +1,61 @@
-import { Component } from '@angular/core';
+
+import { AfterViewInit, Component, EventEmitter, Inject, Input, OnChanges, OnDestroy, Output, SimpleChanges, ViewChild } from '@angular/core';
+import { ClientModelTable } from '../../client.models';
+import { MatTableDataSource, MatTableModule,  } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { Subscription } from 'rxjs';
+import { SERVICES_TOKEN } from '../../../services/service.token';
+import { IDialogManagerService } from '../../../services/idialog-manager.service';
+import { DialogManagerService } from '../../../services/dialog-manager.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-client-table',
-  imports: [],
+  imports: [MatTableModule, MatIconModule, MatPaginatorModule],
   templateUrl: './client-table.component.html',
-  styleUrl: './client-table.component.scss'
+  styleUrl: './client-table.component.scss',
+  providers: [{ provide: SERVICES_TOKEN.DIALOG, useClass: DialogManagerService }]
 })
-export class ClientTableComponent {
+export class ClientTableComponent implements AfterViewInit, OnChanges, OnDestroy {
+
+  @Input() clients: ClientModelTable[] = []
+
+  dataSource!: MatTableDataSource<ClientModelTable>
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  displayedColumns: string[] = ['name', 'email', 'phone', 'actions']
+
+  private dialogManagerServiceSubscriptions?: Subscription
+
+  @Output() confirmDelete = new EventEmitter<ClientModelTable>()
+
+  @Output() requestUpdate = new EventEmitter<ClientModelTable>()
+
+  constructor(
+    @Inject(SERVICES_TOKEN.DIALOG) private readonly dialogManagerService: IDialogManagerService,
+  ) { }
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['clients'] && this.clients) {
+      this.dataSource = new MatTableDataSource<ClientModelTable>(this.clients)
+      if (this.paginator) {
+        this.dataSource.paginator = this.paginator
+      }
+    }
+  }
+  ngOnDestroy(): void {
+    if (this.dialogManagerServiceSubscriptions) {
+      this.dialogManagerServiceSubscriptions.unsubscribe()
+    }
+  }
+
+  formatPhone(phone: string) {
+    return `( ${phone.substring(0, 2)} ) ${phone.substring(2, 7)} - ${phone.substring(7)}`
+  }
 
 }
