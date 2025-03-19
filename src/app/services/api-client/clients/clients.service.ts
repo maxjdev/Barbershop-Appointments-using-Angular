b3446e9461d83f5ce5c96508c778c9abdcc35837
maxjdev <maxjramosdev@gmail.com>
2025-03-19 20:36:06 -0300

    feat/enhance client management with form integration and service updates

--- src/app/services/api-client/clients/clients.service.ts
+++ src/app/services/api-client/clients/clients.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { IClientService } from './iclients.service';
+import { ICLientService } from './iclients.service';
 import { Observable } from 'rxjs';
 import { SaveClientRequest, SaveClientResponse, UpdateClientRequest, UpdateClientResponse, ListClientResponse, DetailClientResponse } from './client.models';
 import { HttpClient } from '@angular/common/http';
@@ -8,30 +8,25 @@ import { environment } from '../../../../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
-export class ClientsService implements IClientService {
+export class ClientsService implements ICLientService {
 
-  private readonly basePath = environment.apiUrl;
+  private readonly basePath = environment.apiUrl
 
-  constructor(private http: HttpClient) { };
+  constructor(private http: HttpClient) { }
 
   save(request: SaveClientRequest): Observable<SaveClientResponse> {
-    return this.http.post<SaveClientResponse>(`${this.basePath}clients`, request);
+    return this.http.post<SaveClientResponse>(`${this.basePath}clients`, request)
   }
-
   update(id: number, request: UpdateClientRequest): Observable<UpdateClientResponse> {
-    return this.http.put<UpdateClientResponse>(`${this.basePath}clients${id}`, request);
+    return this.http.put<UpdateClientResponse>(`${this.basePath}clients/${id}`, request)
   }
-
   delete(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.basePath}clients${id}`);
+    return this.http.delete<void>(`${this.basePath}clients/${id}`)
   }
-
   list(): Observable<ListClientResponse[]> {
-    return this.http.get<ListClientResponse[]>(`${this.basePath}clients`);
+    return this.http.get<ListClientResponse[]>(`${this.basePath}clients`)
   }
-
   findById(id: number): Observable<DetailClientResponse> {
-    return this.http.get<DetailClientResponse>(`${this.basePath}clients${id}`);
+    return this.http.get<DetailClientResponse>(`${this.basePath}clients/${id}`)
   }
-
 }
--- src/app/services/api-client/clients/iclients.service.ts
+++ src/app/services/api-client/clients/iclients.service.ts
@@ -1,16 +1,16 @@
 import { Observable } from "rxjs";
 import { DetailClientResponse, ListClientResponse, SaveClientRequest, SaveClientResponse, UpdateClientRequest, UpdateClientResponse } from "./client.models";
 
-export interface IClientService {
+export interface ICLientService {
 
-  save(request: SaveClientRequest): Observable<SaveClientResponse>;
+    save(request: SaveClientRequest): Observable<SaveClientResponse>
 
-  update(id:number, request: UpdateClientRequest): Observable<UpdateClientResponse>;
+    update(id: number, request: UpdateClientRequest): Observable<UpdateClientResponse>
 
-  delete(id: number): Observable<void>;
+    delete(id: number): Observable<void>
 
-  list(): Observable<ListClientResponse[]>;
+    list(): Observable<ListClientResponse[]>
 
-  findById(id: number): Observable<DetailClientResponse>;
+    findById(id: number): Observable<DetailClientResponse>
 
 }
